3bd2d61f1ebd6777046e468b4821f7e0256ffc48
keenwon <semanwmj@gmail.com>
2017-07-11 22:59:13 +0800

    支持配置功能

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 /**
  * 入口
  */
-function main(word) {
+function main(word, configs) {
   if (!word) {
     return Promise.reject(new Error('请输入要查询的文字'));
   }
@@ -20,7 +20,7 @@ function main(word) {
 
   const url = `http://cn.bing.com/dict/search?q=${keyword}`;
 
-  return fetch(url)
+  return fetch(url, configs)
     .then(body => parser(body))
     .catch(error => {
       if (error.name === 'FetchError') {
